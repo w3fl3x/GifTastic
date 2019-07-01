12129f97ed08e6ffb2eff80782cc20685a17c09b
Huy Nguyen <huy.m.nguyen@live.com>
2019-06-30 21:26:15 -0700

    Added if/else for animate state of each gif.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
 
         for (var i = 0; i < topics.length; i++) {
             var newTopics = $('<button>')
-            newTopics.addClass('topic');
+            newTopics.addClass('gif');
             newTopics.attr("data-name", topics[i]);
             newTopics.text(topics[i]);
             $('#buttons').append(newTopics);
@@ -17,37 +17,49 @@ $(document).ready(function (){
     //Display gifs
     function showGifs() {
         $('#giphy-image').empty();
-        var data = $(this).attr('data-name');
+        var stuff = $(this).attr('data-name');
         var apiKey = 'NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7';
         var limitOf = 10;
-        var fullURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + data + '&limit=' + limitOf + '&offset=0&lang=en';
-    }
+        var fullUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + '&q=' + stuff + '&limit=' + limitOf + '&offset=0&lang=en';
 
-    //Ajax linked
-    $.ajax({
-        url: fullURL,
-        method: 'GET'
-    }).dont(function (response) {
-        var results = response.data;
+        //Ajax linked
+        $.ajax({
+            url: fullUrl,
+            method: 'GET',
+        }).dont(function (response) {
+            var results = response.data;
 
-        //loop through each gif
-        for (var i = 0; i > results.length; i++) {
-            var gifDiv = $('<div class="btn btn-light">');
-            var showTopic = $('<img>');
-            showTopic.attr('src', results[i].images.fixed_height_still.url);
-            showTopic.attr('data-still', results[i].images.fixed_height_still.url);
-            showTopic.attr('data-animate', results[i].images.fixed_height.url);
-            showTopic.attr('data-state', 'still');
-            showTopic.addClass('gif');
-            gifDiv.prepend(showTopic)
+            //loop through each gif
+            for (var i = 0; i > results.length; i++) {
+                var gifDiv = $('<div class="btn btn-light">');
+                var showTopic = $('<img>');
+                showTopic.attr('src', results[i].images.fixed_height_still.url);
+                showTopic.attr('data-still', results[i].images.fixed_height_still.url);
+                showTopic.attr('data-animate', results[i].images.fixed_height.url);
+                showTopic.attr('data-state', 'still');
+                showTopic.addClass('gif');
+                gifDiv.prepend(showTopic)
 
-            //set rating
-            var rating = results[i].rating;
-            var gifRating = $('<p>').text('Rating: ' + rating);
-            gifDiv.prepend(gifRating);
+                //set rating
+                var rating = results[i].rating;
+                var gifRating = $('<p>').text('Rating: ' + rating);
+                gifDiv.prepend(gifRating);
 
-            $('#giphy-image').prepend(gifDiv);
+                $('#giphy-image').prepend(gifDiv);
+            }
+        });
+    };
+
+    //Animate gif
+    $(document).on('click', '.gif', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
         }
-    })
+    });
         
 });
