6d72fdad405568ccd4cd64070d82f0ac8a13850b
Huy Nguyen <huy.m.nguyen@live.com>
2019-06-30 20:42:17 -0700

    Function to create button in JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function (){
+    var topics = ['Walt Disney', 'Hot Wheels', 'Cars', 'Fast'];
 
+    //Creating buttons
+    function createButton() {
+        $('#buttons').empty();
+
+        for (var i = 0; i < topics.length; i++) {
+            var newTopics = $('<button>')
+            newTopics.addClass('topic');
+            newTopics.attr("data-name", topics[i]);
+            newTopics.text(topics[i]);
+            $('#buttons').append(newTopics);
+        }
+    }
+
+    //Display gifs
+
+
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7g&limit=10";
+        
 });
