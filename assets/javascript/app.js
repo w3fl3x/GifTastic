abcf0ffde74a1e53404fb92f2d3a2b4905438ca7
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-01 04:02:33 -0700

    Fixed spelling in JS.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function() {
     var topics = ['Walt Disney', 'Hot Wheels', 'Cars', 'Fast'];
 
     //Creating buttons
@@ -18,32 +18,32 @@ $(document).ready(function (){
     function showGifs() {
         $('#giphy-image').empty();
         var stuff = $(this).attr('data-name');
-        var apiKey = 'NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7';
-        var limitOf = 10;
-        var fullUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + '&q=' + stuff + '&limit=' + limitOf + '&offset=0&lang=en';
+        var apiKey = 'NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7g';
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + '&q=' + stuff + '&limit=10&offset=0&lang=en';
 
         //Ajax linked
         $.ajax({
-            url: fullUrl,
-            method: 'GET',
-        }).dont(function (response) {
+            url: queryUrl,
+            method: 'GET'
+        })
+            .then(function (response) {
             var results = response.data;
 
             //loop through each gif
             for (var i = 0; i > results.length; i++) {
-                var gifDiv = $('<div class="btn btn-light">');
+                var gifDiv = $('<div class="button button4>');
                 var showTopic = $('<img>');
                 showTopic.attr('src', results[i].images.fixed_height_still.url);
                 showTopic.attr('data-still', results[i].images.fixed_height_still.url);
                 showTopic.attr('data-animate', results[i].images.fixed_height.url);
                 showTopic.attr('data-state', 'still');
-                showTopic.addClass('gif');
-                gifDiv.prepend(showTopic)
+                showTopic.addClass('gifs');
+                gifDiv.append(showTopic)
 
                 //set rating
                 var rating = results[i].rating;
                 var gifRating = $('<p>').text('Rating: ' + rating);
-                gifDiv.prepend(gifRating);
+                gifDiv.append(gifRating);
 
                 $('#giphy-image').prepend(gifDiv);
             }
@@ -61,5 +61,18 @@ $(document).ready(function (){
             $(this).attr('data-state', 'still');
         }
     });
+
+    //Add new button for gif topic
+    $('#submitButton').on('click', function() {
+        var stuff = $('#userinput').val().trim();
+        topics.push(stuff);
+        form.reset();
+        createButton();
+        return false;
+    });
+
+
+    $(document).on('click', '.gif', showGifs);
+    createButton()
         
 });
