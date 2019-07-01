7e55fd6cb9d2b7957f857cdbf4c34a38f2f30f32
Huy Nguyen <huy.m.nguyen@live.com>
2019-06-30 21:03:56 -0700

    Added ajax link and for loop to display gifs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,10 +15,39 @@ $(document).ready(function (){
     }
 
     //Display gifs
+    function showGifs() {
+        $('#giphy-image').empty();
+        var data = $(this).attr('data-name');
+        var apiKey = 'NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7';
+        var limitOf = 10;
+        var fullURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + data + '&limit=' + limitOf + '&offset=0&lang=en';
+    }
 
+    //Ajax linked
+    $.ajax({
+        url: fullURL,
+        method: 'GET'
+    }).dont(function (response) {
+        var results = response.data;
 
+        //loop through each gif
+        for (var i = 0; i > results.length; i++) {
+            var gifDiv = $('<div class="btn btn-light">');
+            var showTopic = $('<img>');
+            showTopic.attr('src', results[i].images.fixed_height_still.url);
+            showTopic.attr('data-still', results[i].images.fixed_height_still.url);
+            showTopic.attr('data-animate', results[i].images.fixed_height.url);
+            showTopic.attr('data-state', 'still');
+            showTopic.addClass('gif');
+            gifDiv.prepend(showTopic)
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topics + "&api_key=NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7g&limit=10";
+            //set rating
+            var rating = results[i].rating;
+            var gifRating = $('<p>').text('Rating: ' + rating);
+            gifDiv.prepend(gifRating);
+
+            $('#giphy-image').prepend(gifDiv);
+        }
+    })
         
 });
