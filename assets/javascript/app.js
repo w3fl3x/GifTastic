3be0686b7fe67b5ff7d0fee3400748dcd3415f04
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-03 17:17:17 -0700

    Changed id in JS to match with html. Got buttons to pull gif and function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,68 @@
-$(document).ready(function() {
-    var topics = ['Walt Disney', 'Hot Wheels', 'Cars', 'Fast'];
+$(document).ready(function () {
+    // var movies = ['iron man', 'the incredible hulk', 'thor', 'captain america: the first avenger', 'the avengers', 'guardians of the galaxy', 'ant man', 'black panther', 'avengers endgame'];
+    var movies = ['funny', 'reaction', 'poking', 'fail', 'cactus'];
 
-    //Creating buttons
-    function createButton() {
-        $('#buttons').empty();
-
-        for (var i = 0; i < topics.length; i++) {
-            var newTopics = $('<button>')
-            newTopics.addClass('gif');
-            newTopics.attr("data-name", topics[i]);
-            newTopics.text(topics[i]);
-            $('#buttons').append(newTopics);
+    //Add buttons for topics array
+    function renderButtons() {
+        $('#movie-buttons').empty();
+        for (i = 0; i < movies.length; i++) {
+            $('#movie-buttons').append('<button class="button button4" data-movie="' + movies[i] + '">' + movies[i] + '</button>');
         }
     }
 
-    //Display gifs
-    function showGifs() {
-        $('#giphy-image').empty();
-        var stuff = $(this).attr('data-name');
-        var apiKey = 'NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7g';
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + '&q=' + stuff + '&limit=10&offset=0&lang=en';
-
-        //Ajax linked
-        $.ajax({
-            url: queryUrl,
-            method: 'GET'
-        })
-            .then(function (response) {
-            var results = response.data;
-
-            //loop through each gif
-            for (var i = 0; i > results.length; i++) {
-                var gifDiv = $('<div class="button button4>');
-                var showTopic = $('<img>');
-                showTopic.attr('src', results[i].images.fixed_height_still.url);
-                showTopic.attr('data-still', results[i].images.fixed_height_still.url);
-                showTopic.attr('data-animate', results[i].images.fixed_height.url);
-                showTopic.attr('data-state', 'still');
-                showTopic.addClass('gifs');
-                gifDiv.append(showTopic)
-
-                //set rating
-                var rating = results[i].rating;
-                var gifRating = $('<p>').text('Rating: ' + rating);
-                gifDiv.append(gifRating);
-
-                $('#giphy-image').prepend(gifDiv);
-            }
-        });
-    };
-
-    //Animate gif
-    $(document).on('click', '.gif', function() {
-        var state = $(this).attr('data-state');
-        if (state == 'still') {
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
-
-    //Add new button for gif topic
-    $('#submitButton').on('click', function() {
-        var stuff = $('#userinput').val().trim();
-        topics.push(stuff);
+    renderButtons();
+    
+    //Adding button for movie entered
+    $('#add-movie').on('click', function () {
+        event.preventDefault();
+        var movie = $('#movie-input').val().trim();
+        movies.push(movie);
         form.reset();
-        createButton();
+        renderButtons();
         return false;
     });
 
+    //Getting gifs from api and onto html
+    $('button').on('click', function () {
+        var movie = $(this).attr('data-movie');
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + movie + '&api_key=NqboVSH8zAAnvT3rvSVfDCicRHLH4Y7g&limit=10&offset=0&lang=en'
 
-    $(document).on('click', '.gif', showGifs);
-    createButton()
-        
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function (response) {
+            var results = response.data;
+            $('#movies').empty();
+            for (var i = 0; i < results.length; i++) {
+                var movieDiv = $('<div>');
+                var h6 = $('<h6>').text('Rating: ' + results[i].rating);
+                var movieImg = $('<img>');
+                
+                movieImg.attr('src', results[i].images.original_still.url);
+                movieImg.attr('data-still', results[i].images.original_still.url);
+                movieImg.attr('data-animate', results[i].images.original.url);
+                movieImg.attr('data-state', 'still');
+                movieImg.attr('class', 'gif');
+                movieDiv.append(h6);
+                movieDiv.append(movieImg);
+                $('#movies').append(movieDiv);
+            }
+        });
+    });
+
+    function changeState() {
+        var state = $(this).attr('data-state');
+        var animateImage = $(this).attr('data-animate');
+        var stillImage = $(this).attr('data-still');
+
+        if (state === 'still') {
+            $(this).attr('src', animateImage);
+            $(this).attr('data-state', 'animate');
+        } else if (state === 'animate') {
+            $(this).attr('src', stillImage);
+            $(this).attr('data-state', 'still');
+        }
+    }
+
+    $(document).on('click', '.gif', changeState);
 });
